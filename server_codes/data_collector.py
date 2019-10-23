23570e04dd85073c0783efe6429e775599f3a58a
Andy Mao <andy@unraveldata.com>
2019-10-23 12:12:01 -0700

    update data collector

--- server_codes/data_collector.py
+++ server_codes/data_collector.py
@@ -1,9 +1,11 @@
 import paho.mqtt.client as mqtt
 from elasticsearch import Elasticsearch
 from datetime import datetime
+import traceback
+import json
 
-mqtt_server = 'localhost'
-elastic_server = 'localhost'
+mqtt_server = 'localhost' #localhost
+elastic_server = 'localhost' #localhost
 
 
 class DataPoint:
@@ -28,6 +30,31 @@ def add_doc(msg):
         body['raw_moist'] = str(msg.payload.decode("utf-8"))
     elif msg.topic.endswith('temp'):
         body['raw_temp'] = str(msg.payload.decode("utf-8"))
+    elif msg.topic.startswith('sensor_data'):
+        body['device_id'] = msg.topic.split('/')[1]
+        try:
+            """ {"soil_moist": "", "raw_temp": "", "air_hum": "", "air_temp": "", "ip_addr": ""} """
+            result = json.loads(str(msg.payload.decode("utf-8")))
+            body['moisture'] = analog_to_percent(result.get("soil_moist", 0))
+            body['raw_moisture'] = result.get("soil_moist", 0)
+            body['raw_temp'] = result.get("raw_temp", 0)
+            body['air_humidity'] = result.get("air_hum", 0)
+            body['air_temp'] = result.get("air_temp", 0)
+            body['ip_address'] = result.get("ip_addr", '0.0.0.0')
+        except:
+            traceback.print_exc()
+    else:
+        try:
+            """ {"soil_moist": "", "raw_temp": "", "air_hum": "", "air_temp": "", "ip_addr": ""} """
+            result = json.loads(str(msg.payload.decode("utf-8")))
+            body['moisture'] = analog_to_percent(result.get("soil_moist", 0))
+            body['raw_moisture'] = result.get("soil_moist", 0)
+            body['raw_temp'] = result.get("raw_temp", 0)
+            body['air_humidity'] = result.get("air_hum", 0)
+            body['air_temp'] = result.get("air_temp", 0)
+            body['ip_address'] = result.get("ip_addr", '0.0.0.0')
+        except:
+            traceback.print_exc()
     es.index(index=index, body=body)
 
 
@@ -37,29 +64,31 @@ def on_log(client, userdata, level, buf):
 
 def on_message(client, userdata, message):
     global moist, temp, temp_plt, moist_plt, fig
+    reading = message.payload.decode("utf-8")
+    print("message received ", str(reading))
+    print("message topic=", message.topic)
+    print("message qos=", message.qos)
+    print("message retain flag=", message.retain)
     if message.topic.endswith('moist'):
-        moist_reading = message.payload.decode("utf-8")
-        print("message received ", str(moist_reading))
-        print("message topic=", message.topic)
-        print("message qos=", message.qos)
-        print("message retain flag=", message.retain)
         print(datetime.utcnow())
-        if int(moist_reading) > 0:
+        if int(reading) > 0:
             add_doc(message)
     elif message.topic.endswith('temp'):
         print("message received ", str(message.payload.decode("utf-8")))
         print("message topic=", message.topic)
         add_doc(message)
+    else:
+        add_doc(message)
 
 
 def analog_to_percent(analog):
     min_reading = 1750
-    stepper = 35
+    stepper = 32
     return 100 - (int(analog) - min_reading) / stepper
 
-client = mqtt.Client('receiver')
+client = mqtt.Client('data_receiver')
 client.connect(mqtt_server)
-# client.on_log=on_log
-client.on_message=on_message
-client.subscribe("#")
+client.on_log = on_log
+client.on_message = on_message
+client.subscribe([("+", 0), ("sensor_data/+", 0)])
 client.loop_forever()
